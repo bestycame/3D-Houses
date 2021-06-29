efedf20aa0c361f009f722c3979f3e61259df665
Olivier d'Otreppe <bestycame@gmail.com>
2021-06-29 12:01:24 +0200

    Initial try at making a module

---
+++ becode3d/variables.py
@@ -0,0 +1,14 @@
+MAPBOX_KEY = 'pk.eyJ1IjoiYmFjYXlhdzU1OSIsImEiOiJja2dtNTgyNW8wMWN2MnBzM2loNGt4NmlzIn0.3IYulU7qzfpq2Ms0aaYWMQ'
+
+DATAS = {'LGE': {'MNT': '/media/bestycame/407C5CD57C5CC776/3D/RELIEF_WALLONIE_MNT_2013_2014_LIEGE.tif',
+                 'MNS': '/media/bestycame/407C5CD57C5CC776/3D/RELIEF_WALLONIE_MNS_2013_2014_LIEGE.tif'},
+         'HAI': {'MNT': '/mnt/3D/RELIEF_WALLONIE_MNT_2013_2014_HAINAUT.tif',
+                 'MNS': '/mnt/3D/RELIEF_WALLONIE_MNS_2013_2014_HAINAUT.tif'},
+         'LUX': {'MNT': '/mnt/3D/RELIEF_WALLONIE_MNT_2013_2014_LUXEMBOURG.tif',
+                 'MNS': '/mnt/3D/RELIEF_WALLONIE_MNS_2013_2014_LUXEMBOURG.tif'},
+         'NAM': {'MNT': '/mnt/3D/RELIEF_WALLONIE_MNT_2013_2014_NAMUR.tif',
+                 'MNS': '/mnt/3D/RELIEF_WALLONIE_MNS_2013_2014_NAMUR.tif'},
+         'BRA': {'MNT': '/mnt/3D/RELIEF_WALLONIE_MNT_2013_2014_BRABANT.tif',
+                 'MNS': '/mnt/3D/RELIEF_WALLONIE_MNS_2013_2014_BRABANT.tif'}}
+
+BATI_3D = '../BATI3D_2013-2014.gdb'
