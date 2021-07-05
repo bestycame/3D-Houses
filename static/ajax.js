2a0b1d894745b07360466828a3968b7801f0c6a0
Olivier d'Otreppe <bestycame@gmail.com>
2021-07-05 13:25:05 +0200

    Final details

--- static/ajax.js
+++ static/ajax.js
@@ -3,9 +3,15 @@ document.querySelector('.plotly-graph-div').on('plotly_click', function(event){
       trace_id = event['points'][0]['curveNumber']
       document.getElementById('alert').style.display = "none"
       document.getElementById("h2").innerHTML = ""
+      console.log()
+      const card = `<div class="card" id="card">
+                        <div class="card_upper">Latitude / Longitude (L72)</div>
+                        <div class="card_lower">${event['points'][0].y} / ${event['points'][0].x}</div>
+                     </div>`
+                  h2.insertAdjacentHTML("beforeend", card)
       new_data = features[trace_id - 1]
       for (const [key, value] of Object.entries(new_data)) {
-                    const card = `  <div class="card" id="card">
+                    const card = `<div class="card" id="card">
                                     <div class="card_upper">${key}</div>
                                     <div class="card_lower">${value}</div>
                                   </div>`
